bfc1697c907a92185c2146951c6755dbd9b06156
Dheeraj Gupta <dj13042002@gmail.com>
2022-01-09 12:21:46 +0530

    Added engines

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let app = express();
 
 app.use(express.static("public"));
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 let server = app.listen(port, ()=>{
     console.log("Listening to server "+port);
 });
